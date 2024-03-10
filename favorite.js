4946097497eb112c89f01dd16be443218bbb86b3
Amber Wang <wangshengyan6@gmail.com>
2024-03-10 19:10:59 +0800

    removeFromFavorite

--- favorite.js
+++ favorite.js
@@ -1,16 +1,32 @@
 const BASE_URL = "https://webdev.alphacamp.io";
 const INDEX_URL = BASE_URL + "/api/movies/";
 const POSTER_URL = BASE_URL + "/posters/";
+// 電影的資料是從 localStorage 取得
 const movies = JSON.parse(localStorage.getItem("favoriteMovies"));
 const dataPanel = document.querySelector("#data-panel");
 
+// 刪除收藏電影
+function removeFromFavorite(id) {
+  // 要使用 splice 方法刪除，所以需要知道元素的 index
+  const movieIndex = movies.findIndex((movie) => movie.id === id);
+  // 若找不到，則結束函式
+  if (movieIndex === -1) return;
+  // 刪除該筆電影
+  movies.splice(movieIndex, 1);
+
+  //   存回localStorage
+  localStorage.setItem("favoriteMovies", JSON.stringify(movies));
+
+  //   更新頁面
+  renderMovieList(movies);
+}
 // 3.點擊模態框監聽事件 6.添加else if收藏事件
 dataPanel.addEventListener("click", function onPanelClicked(event) {
   // matches方法判斷被點擊的元素有無特定【類】 ，若有則返回true，否則為false
   if (event.target.matches(".btn-show-movie")) {
     showMovieModal(Number(event.target.dataset.id));
-  } else if (event.target.matches(".btn-add-favorite")) {
-    addToFavorite(Number(event.target.dataset.id));
+  } else if (event.target.matches(".btn-remove-favorite")) {
+    removeFromFavorite(Number(event.target.dataset.id));
   }
 });
 
@@ -33,9 +49,9 @@ function renderMovieList(data) {
                         <button class="btn btn-primary btn-show-movie" data-bs-toggle="modal" data-bs-target="#movie-modal" data-id="${
                           item.id
                         }">More</button>
-                        <button class="btn btn-info btn-add-favorite" data-id="${
+                        <button class="btn btn-danger btn-remove-favorite" data-id="${
                           item.id
-                        }">+</button>
+                        }">x</button>
                     </div>
                 </div>
                 </div>
